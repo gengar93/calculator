018ca31d79c07387aaae0028d02b4fadb2f85eb8
Aditya J <aditya.storm@gmail.com>
2018-10-21 22:46:20 +0530

    the size of the tk buttons has been fixed

--- home.py
+++ home.py
@@ -45,7 +45,8 @@ for i in range(3):
     for j in range(3):
         button_val = (2 - i) * 3 + j + 1
         button = tk.Button(numbers_frame, text=button_val, command=functools.partial(_on_press_number, button_val))
-        button.grid(row=i, column=j, sticky=tk.W + tk.E)
+        button.config(height=2, width=4)
+        button.grid(row=i, column=j, sticky=tk.EW)
 
         # add button the the dictionary
         num_buttons_dict[button_val] = button
@@ -53,15 +54,18 @@ for i in range(3):
 # add button zero
 button = tk.Button(numbers_frame, text='0', command=functools.partial(_on_press_number, 0))
 button.grid(row=3, column=1, sticky=tk.W + tk.E)
+button.config(height=2, width=4)
 num_buttons_dict[0] = button
 
 # add backspace
 backspace_button = tk.Button(numbers_frame, text='<<', command=_on_press_backspace)
 backspace_button.grid(row=3, column=2, sticky=tk.W+tk.E)
+backspace_button.config(height=2, width=4)
 
 # add decimal point button
 decimal_button = tk.Button(numbers_frame, text='.', command=_on_press_decimal)
 decimal_button.grid(row=3, column=0, sticky=tk.W+tk.E)
+decimal_button.config(height=2, width=4)
 
 # operations frame
 operations_frame = ttk.LabelFrame(win, text='')
@@ -94,17 +98,23 @@ def _on_press_equals():
 
 ac_button = tk.Button(operations_frame, text='AC', command=_on_press_ac)
 ac_button.grid(row=0, column=0, columnspan=2, sticky=tk.W + tk.E)
+ac_button.config(height=2, width=4)
 # ttk.Style().configure(ac_button, background='#006400')
 
 add_button = tk.Button(operations_frame, text='+', command=_on_press_add)
+add_button.config(height=2, width=4)
 add_button.grid(row=1, column=0)
 subtract_button = tk.Button(operations_frame, text='-', command=_on_press_subtract)
+subtract_button.config(height=2, width=4)
 subtract_button.grid(row=1, column=1)
 multiply_button = tk.Button(operations_frame, text='*', command=_on_press_multiply)
+multiply_button.config(height=2, width=4)
 multiply_button.grid(row=2, column=0)
 divide_button = tk.Button(operations_frame, text='/', command=_on_press_divide)
+divide_button.config(height=2, width=4)
 divide_button.grid(row=2, column=1)
 equals_button = tk.Button(operations_frame, text='=', command=_on_press_equals)
+equals_button.config(height=2, width=4)
 equals_button.grid(row=3, column=0, columnspan=2, sticky=tk.W + tk.E)
 
 win.mainloop()
