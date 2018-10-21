bda2e5c8ad2d4b4eec19d9dd3f13c94f3afd9f95
Aditya J <aditya.storm@gmail.com>
2018-10-21 20:25:43 +0530

    on button press was using lambda and was creating an error. it was changed to partial

--- home.py
+++ home.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import functools
 
 win = tk.Tk()
 win.resizable(False, False)
@@ -19,6 +20,7 @@ expression_box.grid(row=0, column=0)
 numbers_frame = ttk.LabelFrame(win, text='')
 numbers_frame.grid(row=1, column=0)
 
+
 # create the numbers buttons
 def _on_press_number(number):
     """
@@ -26,15 +28,20 @@ def _on_press_number(number):
     :return:
     """
     # TODO requires the logic for button press
-    pass
+    current_exp_str = expression_str.get()
+    new_exp_str = current_exp_str + '{}'.format(number)
+    expression_str.set(new_exp_str)
 
-num_buttons_list = []
+
+num_buttons_dict = {}
 
 for i in range(3):
     for j in range(3):
-        button_val = (2-i) * 3 + j + 1
-        button = ttk.Button(numbers_frame, text=button_val, command=lambda: _on_press_number(button_val))
+        button_val = (2 - i) * 3 + j + 1
+        button = ttk.Button(numbers_frame, text=button_val, command=functools.partial(_on_press_number, button_val))
         button.grid(row=i, column=j)
 
+        # add button the the dictionary
+        num_buttons_dict[button_val] = button
 
 win.mainloop()
